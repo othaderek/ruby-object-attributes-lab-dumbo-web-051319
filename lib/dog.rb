51f0a79867755d9a2bb0d340762dc9dd573eb1fe
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-15 12:50:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,20 @@
 class Dog
 
-  def name(name)
-    @name = name
+  def name
+    @name
   end
 
   def name=(new_name)
     @name = new_name
   end
+  
+  def breed
+  @breed
+  end
+
+  def breed=(new_name)
+    @breed = new_name
+  end
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,19 @@
 class Person
 
-  def name(name)
-    @name = name
+  def name
+    @name
   end
 
   def name=(new_name)
     @name = new_name
   end
 
-  def job(job)
-    @job = job
+  def job
+    @job
   end
 
   def job=(new_job)
     @job = new_job
+  end
+  
 end
